d9cbcb7bc1a1f9134995ac2da99b50f6270e0019
ayzaz027-alt <ayzaz027@gmail.com>
2025-12-09 23:37:20 +0000

    Update App.tsx

--- frontend/public/src/App.tsx
+++ frontend/public/src/App.tsx
@@ -1,1 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Board from './pages/Board';
+import PrivateRoute from './components/PrivateRoute';
+import './index.css';
 
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+          <Route path="/board/:id" element={
+            <PrivateRoute>
+              <Board />
+            </PrivateRoute>
+          } />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
